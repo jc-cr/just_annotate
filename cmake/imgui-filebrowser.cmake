ed85bd5891d7fe156f962ce582e60c815a9948ad
jc <juan.cruz.business@gmail.com>
2024-08-26 21:42:23 -0500

    Updated cmake spec

--- cmake/imgui-filebrowser.cmake
+++ cmake/imgui-filebrowser.cmake
@@ -1,8 +1,9 @@
 include(FetchContent)
+
 FetchContent_Declare(
-    imgui-filebrowser
-    GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser.git
-    GIT_TAG 17a87fedfa997fb2b504a6b3088cc44e64fcac6a # May 31, 2024
-    GIT_SHALLOW 1
+  imgui-filebrowser
+  GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser
+  GIT_TAG master
 )
+
 FetchContent_MakeAvailable(imgui-filebrowser)
